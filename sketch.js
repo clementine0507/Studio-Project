d2a6b0709fc3aee989c2e6277f2138505056aebf
Clementine <wtan387@aucklanduni.ac.nz>
2025-02-06 13:47:51 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,21 +1,19 @@
 function setup() {
   createCanvas(800, 800);
-  background(255); // white background
+  background(255);
 }
 
-function draw() {
-}
 
 function mousePressed() {
-  let lineCount = int(random(1, 6)); // Randomly generate 1~5 lines
-
-  for (let i = 0; i < lineCount; i++) {
-    let x1 = random(width);
-    let y1 = random(height);
+  background(255);
+  let x1 = random(width);
+  let y1 = random(height);
+ 
+  for (let o = 0; o < 10; o++) {
     let x2 = random(width);
     let y2 = random(height);
-    
-    stroke(0); // black lines
+   
+    stroke(0);
     line(x1, y1, x2, y2);
   }
 }
