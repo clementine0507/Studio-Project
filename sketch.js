75f014ac0fe684eb2c901bed4a8f7f2e644c6871
Clementine <wtan387@aucklanduni.ac.nz>
2025-02-16 20:12:05 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,31 +1,66 @@
-let points = []; // Stores the main points to connect lines
-let lines = [];  // Stores the lines to be drawn
+let mic;
+let isGameStarted = false;
+let soundLevel = 0;
+let linePosition; 
+let previousPosition; 
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight); 
   background(255);
-}
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  fill(0);
 
-function mousePressed() {
-  let newPoint = createVector(mouseX, mouseY);
-  points.push(newPoint); // Store new clicked point
+  text('Sound is your paintbrush.', width / 2, height / 2);
 
-  // Generate lines connecting the new point to previous points
-  for (let p of points) {
-    if (p !== newPoint) { 
-      lines.push({ start: newPoint, end: p });
-    }
-  }
+  mic = new p5.AudioIn();
   
-  redraw(); // Refresh canvas to draw new lines
+  userStartAudio().then(() => {
+    mic.start();
+  })
+
+  linePosition = createVector(width / 2, height / 2); 
+  previousPosition = linePosition.copy(); 
 }
 
 function draw() {
-  background(255);
-  stroke(0);
-  
-  // Draw stored lines
-  for (let l of lines) {
-    line(l.start.x, l.start.y, l.end.x, l.end.y);
+  if (isGameStarted) {
+    soundLevel = mic.getLevel();
+
+    if (soundLevel > 0.01) {
+      updateLinePosition();
+      drawLine();
+    }
   }
+}
+
+function mousePressed() {
+  if (!isGameStarted) {
+    isGameStarted = true;
+    background(255);
+  }
+}
+
+function updateLinePosition() {
+  let angle = random(TWO_PI);  
+  let speed = map(soundLevel, 0, 1, 10, 50);  
+
+  linePosition.x += cos(angle) * speed;
+  linePosition.y += sin(angle) * speed;
+
+  linePosition.x = constrain(linePosition.x, 0, width);
+  linePosition.y = constrain(linePosition.y, 0, height);
+}
+
+function drawLine() {
+  let r = random(0, 0); 
+  let g = random(150, 255); 
+  let b = random(150, 255); 
+  stroke(r, g, b); 
+  strokeWeight(map(soundLevel, 0, 1, 2, 15)); 
+  
+  line(previousPosition.x, previousPosition.y, linePosition.x, linePosition.y);
+
+  previousPosition.x = linePosition.x;
+  previousPosition.y = linePosition.y;
 }
